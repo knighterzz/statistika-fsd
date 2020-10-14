9000b4bdf870d2acd955330a276e863c54bfdf1b
knighterzz <56337946+knighterzz@users.noreply.github.com>
2020-10-14 23:53:48 +0700

    Add files via upload

---
+++ Probabilitas.r
@@ -0,0 +1,11 @@
+#19523096
+#Daffa Nayaka Bagaskara Jusuf
+#FSD - Probabilitas
+
+# Contoh kasus: Dalam sebuah gim terdapat fitur gacha yang mengandalkan keberuntungan.
+# Untuk mendapatkan barang langka dalam suatu gim diberikan 7% kemungkinan dalam setiap pembelian surprise box.
+# Jika Affandal membeli 10 surprise box dalam satu waktu.
+# Berapa kemungkinan Affandal mendapat 3 barang langka?
+
+# Diketahui ==> x = 3, n = 10, p = 7%
+dbinom(3,  10, 0.07, FALSE)
